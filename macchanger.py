5a9b1d0b297e9b6acb3f054e244fb996d4770443
Kretinga <111123256+Kretinga@users.noreply.github.com>
2023-01-16 17:42:59 +0100

    Create macchanger.py

---
+++ macchanger.py
@@ -0,0 +1,27 @@
+import subprocess
+import optparse
+
+def get_arguments():
+
+        parser = optparse.OptionParser()
+        parser.add_option("-i", "--interface", dest = "interface", help="Parameter to enter your interface")
+        parser.add_option("-m", "--mac", dest = "new_mac", help="Interface to change your MAC Direction")
+
+        (options, arguments) = parser.parse_args()
+        if not options.interface:
+                parser.error("[!] Enter a interface, use --help for more information")
+        elif not options.new_mac:
+                parser.error("[!] Enter a MAC Direction, use --help for more information")
+        return options
+
+def change_mac(interface, new_mac):
+
+        print("[!] Changing MAC Direction to " + interface + " to " + new_mac)
+
+        subprocess.call(["ifconfig", interface, "down"])
+        subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+        subprocess.call(["ifconfig", interface, "up"])
+
+options = get_arguments()
+change_mac(options.interface,options.new_mac)
+print("\n\n[+] MAC Changed correctly! \n")
